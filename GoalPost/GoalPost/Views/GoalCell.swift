a96de3a2a18d6ed8f8bb28dd3d4387bee97eeb77
Benjamin Kimble <benjaminroykimble@gmail.com>
2017-10-20 17:36:28 -0600

    Set Up Goal Cell and CoreData
    - Set up the GoalCellVC class
    - Created UI for Goal Cells
    - Created Core Data model for Goals

---
+++ GoalPost/GoalPost/Views/GoalCell.swift
@@ -0,0 +1,31 @@
+//
+//  GoalCell.swift
+//  GoalPost
+//
+//  Created by Benjamin Kimble on 10/20/17.
+//  Copyright © 2017 Benjamin Kimble. All rights reserved.
+//
+
+import UIKit
+
+class GoalCell: UITableViewCell {
+
+    //@IBOutlets
+    @IBOutlet weak var descriptionLbl: UILabel!
+    @IBOutlet weak var typeLbl: UILabel!
+    @IBOutlet weak var progressLbl: UILabel!
+    
+    //My Functions
+    func configureCell(description: String, type: String, progressAmount: Int) {
+        self.descriptionLbl.text = description
+        self.typeLbl.text = type
+        self.progressLbl.text = String(describing: progressAmount)
+    }
+    
+    //System Functions and Overrides
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}

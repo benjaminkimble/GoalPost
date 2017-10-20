20b4eccf5e623f297f50262ffc8f55ece055d718
Benjamin Kimble <benjaminroykimble@gmail.com>
2017-10-20 16:52:13 -0600

    Initialized Project and Set Up GoalVC
    - Initialized Project
    - Set up GoalVC
    - Created UI for Goal View
    - Added constraints for Goal View
    - Set up IBAction for "add goal" button

---
+++ GoalPost/GoalPost/Controllers/GoalsVC.swift
@@ -0,0 +1,36 @@
+//
+//  GoalsVC.swift
+//  GoalPost
+//
+//  Created by Benjamin Kimble on 10/20/17.
+//  Copyright © 2017 Benjamin Kimble. All rights reserved.
+//
+
+import UIKit
+
+class GoalsVC: UIViewController {
+
+    //@IBOutlets
+    @IBOutlet weak var goalTable: UITableView!
+    
+    //@IBActions
+    @IBAction func addGoalBtnPressed(_ sender: Any) {
+        
+    }
+    
+    
+    
+    //System Functions and Overrides
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

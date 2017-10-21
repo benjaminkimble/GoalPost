e23b783abe59393aa8a0708006bf00f00776130f
Benjamin Kimble <benjaminroykimble@gmail.com>
2017-10-21 15:28:33 -0600

    Set Up Saving
    - Added saving functionality to the FinishGoalVC

--- GoalPost/GoalPost/Controllers/FinishGoalVC.swift
+++ GoalPost/GoalPost/Controllers/FinishGoalVC.swift
@@ -7,6 +7,9 @@
 //
 
 import UIKit
+import CoreData
+
+let appDelegate = UIApplication.shared.delegate as? AppDelegate
 
 class FinishGoalVC: UIViewController, UITextFieldDelegate {
 
@@ -20,13 +23,41 @@ class FinishGoalVC: UIViewController, UITextFieldDelegate {
     
     //@IBActions
     @IBAction func createGoalBtnPressed(_ sender: Any) {
-        //pass data into Core Data Goal Model
+        if pointsTextField.text != "" {
+            self.save { complete in
+                if complete {
+                    dismiss(animated: true, completion: nil)
+                }
+            }
+        }
+    }
+    
+    @IBAction func backBtnPressed(_ sender: Any) {
+        dismissDetail()
     }
     
     //My Functions
     func initData(description: String, type: GoalType) {
         self.goalDescription = description
         self.goalType = type
+    }
+    
+    func save(completion: (_ finished: Bool) -> () ) {
+        guard let managedContext = appDelegate?.persistentContainer.viewContext else { return }
+        let goal = Goal(context: managedContext)
+        
+        goal.goalDescription = goalDescription
+        goal.goalType = goalType.rawValue
+        goal.goalAmount = Int32(pointsTextField.text!)!
+        goal.goalProgress = Int32(0)
+        
+        do {
+            try managedContext.save()
+            completion(true)
+        } catch {
+            debugPrint("Could Not Save \(error.localizedDescription)")
+            completion(false)
+        }
     }
     
     //System Functions and Overrides
